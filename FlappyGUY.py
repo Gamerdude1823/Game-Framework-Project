205cd4cd31f925404fc36fec0bf2343935a4de55
Zack Williams <wil18020@byui.edu>
2023-12-13 18:25:16 -0700

    code edits

--- FlappyGUY.py
+++ FlappyGUY.py
@@ -16,9 +16,9 @@ BIRD_SIZE = 40
 GRAVITY = 0.5
 JUMP_HEIGHT = 10
 OBSTACLE_WIDTH = 50
-OBSTACLE_HEIGHT = random.randint(100, 300)
+OBSTACLE_HEIGHT = random.randint(100 , 300)
 OBSTACLE_GAP = 150
-FPS = 60
+FPS = 30
 
 # Colors
 WHITE = (255, 255, 255)
@@ -39,7 +39,7 @@ background_img = pygame.transform.scale(background_img, (WIDTH, HEIGHT))
 
 # Load obstacle images
 obstacle_img = pygame.image.load("F:\Game Framework Project\images\Sprite Cranberry Game - Obstacle Image.png")
-obstacle_img = pygame.transform.scale(obstacle_img, (WIDTH, HEIGHT))
+obstacle_img = pygame.transform.scale(obstacle_img, (OBSTACLE_WIDTH, OBSTACLE_HEIGHT))
 
 
 # Clock to control the frame rate
@@ -52,15 +52,79 @@ obstacle_x = WIDTH
 obstacle_passed = False
 score = 0
 
+# Menu variables
+menu_font = pygame.font.Font(None, 50)
+menu_text = menu_font.render("Press SPACE to Start", True, WHITE)
+menu_text_rect = menu_text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+
+# Function to reset the game state
+def reset_game():
+    global bird_y, bird_velocity, obstacle_x, obstacle_passed, score
+    bird_y = HEIGHT // 2
+    bird_velocity = 0
+    obstacle_x = WIDTH
+    obstacle_passed = False
+    score = 0
+
+bird_rect = pygame.Rect(50, bird_y, BIRD_SIZE, BIRD_SIZE)
+obstacle_rect_top = pygame.Rect(obstacle_x, 0, OBSTACLE_WIDTH, OBSTACLE_HEIGHT)
+obstacle_rect_bottom = pygame.Rect(obstacle_x, OBSTACLE_HEIGHT + OBSTACLE_GAP, OBSTACLE_WIDTH, HEIGHT - OBSTACLE_HEIGHT - OBSTACLE_GAP)
+
+# Function to display the menu
+def show_menu():
+    screen.blit(background_img, (0, 0))
+    screen.blit(menu_text, menu_text_rect)
+    pygame.display.flip()
+
+# Function to handle events in the menu
+def handle_menu_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            return True
+    return False
+
+# Function to handle events in the game
+def handle_game_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            if bird_y > HEIGHT - BIRD_SIZE:
+                return True
+            elif bird_velocity == 0:
+                bird_velocity = -JUMP_HEIGHT
+    return False
+
+# Menu loop
+while True:
+    if handle_menu_events():
+        break
+    show_menu()
+    clock.tick(FPS)
+
 # Game loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                bird_velocity = -JUMP_HEIGHT
+
+    obstacle_x -= 5
+    if obstacle_x < 0:
+        obstacle_x = WIDTH + random.randint(50, 200)  # Randomize obstacle position when it goes off-screen
+        obstacle_height = random.randint(100, 300)
+        obstacle_passed = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE]:
+        if bird_y > HEIGHT - BIRD_SIZE:  # Allow jumping even if not on the ground
+            reset_game()
+        else:
+            bird_velocity = -JUMP_HEIGHT
 
     # Update bird position and velocity
     bird_y += bird_velocity
@@ -76,22 +140,24 @@ while True:
     # Check for collisions with the ground or ceiling
     if bird_y > HEIGHT - BIRD_SIZE or bird_y < 0:
         pygame.display.set_caption('You Crashed! Game Over')
-        pygame.mixer.music.stop
-        time.sleep (5)
-        pygame.quit()
-        sys.exit()
+        pygame.mixer.music.stop()
+        time.sleep (2)
+        reset_game()
+        
 
     # Check for collisions with the obstacle
-    if obstacle_x < BIRD_SIZE < obstacle_x + OBSTACLE_WIDTH:
-        if bird_y < OBSTACLE_HEIGHT or bird_y > OBSTACLE_HEIGHT + OBSTACLE_GAP:
-            pygame.display.set_caption('You Crashed! Game Over')
-            pygame.mixer.music.stop
-            time.sleep (5)
-            pygame.quit()
-            sys.exit()
+    bird_rect = pygame.Rect(50, bird_y, BIRD_SIZE, BIRD_SIZE)
+    obstacle_rect_top = pygame.Rect(obstacle_x, 0, OBSTACLE_WIDTH, OBSTACLE_HEIGHT)
+    obstacle_rect_bottom = pygame.Rect(obstacle_x, OBSTACLE_HEIGHT + OBSTACLE_GAP, OBSTACLE_WIDTH, HEIGHT - OBSTACLE_HEIGHT - OBSTACLE_GAP)
+
+    if bird_rect.colliderect(obstacle_rect_top) or bird_rect.colliderect(obstacle_rect_bottom):
+        pygame.display.set_caption('You Crashed! Game Over')
+        pygame.mixer.music.stop()
+        time.sleep(2)
+        reset_game()
 
     # Check if the bird has passed the obstacle
-    if obstacle_x + OBSTACLE_WIDTH < BIRD_SIZE and not obstacle_passed:
+    if obstacle_x < 50 < obstacle_x + OBSTACLE_WIDTH and not obstacle_passed:
         score += 1
         obstacle_passed = True
 
@@ -102,8 +168,8 @@ while True:
     screen.blit(bird_img, (50, bird_y))
 
     # Draw obstacle
-    pygame.draw.rect(screen, GREEN, (obstacle_x, 0, OBSTACLE_WIDTH, OBSTACLE_HEIGHT))
-    pygame.draw.rect(screen, GREEN, (obstacle_x, OBSTACLE_HEIGHT + OBSTACLE_GAP, OBSTACLE_WIDTH, HEIGHT))
+    screen.blit(obstacle_img, (obstacle_x, 0))
+    screen.blit(obstacle_img, (obstacle_x, OBSTACLE_HEIGHT + OBSTACLE_GAP))
 
     # Display score
     font = pygame.font.Font(None, 36)
@@ -114,4 +180,5 @@ while True:
     pygame.display.flip()
 
     # Set the frame rate
-    clock.tick(FPS)
+    clock.tick(FPS)   
+
--- README.md
+++ README.md
@@ -1,25 +1,23 @@
 # Overview
 
 {Important!  Do not say in this section that this is college assignment.  Talk about what you are trying to accomplish as a software engineer to further your learning.}
+This is a game designed in the style of the famous Flappy Bird that was so popular on the Iphone in early 2016. I have an inside joke with some friends that relates to the Sprite Cranberry advertisment staring Lebron James so i wanted to create a game for them as a holiday gift.
 
-{Provide a description the game that you wrote. Describe how to play your game.}
-
-{Describe your purpose for writing this software.}
+ My game follows the simple idea of Flappy Bird where the user only hasa single input which in this case is the spacebar. The input bounces the sprite in an attempt to avoid falling and incoming obstacles. The score increases with each successful pass. The game is an endurance game so there is no defined end.
 
 {Provide a link to your YouTube demonstration.  It should be a 4-5 minute demo of the game being played and a walkthrough of the code.}
 
 [Software Demo Video](http://youtube.link.goes.here)
+[Music Featured in Game](https://www.youtube.com/watch?v=NJQoXiSD2tg)
 
 # Development Environment
 
-{Describe the tools that you used to develop the software}
-
-{Describe the programming language that you used and any libraries.}
+I utilized the programming langauge Python and the extension Pygame to create this game.
 
 # Useful Websites
 
 {Make a list of websites that you found helpful in this project}
-* [Web Site Name](http://url.link.goes.here)
+* [Stackoverflow - InGame Audio](https://stackoverflow.com/questions/43845800/how-do-i-add-background-music-to-my-python-game)
 * [Web Site Name](http://url.link.goes.here)
 
 # Future Work
